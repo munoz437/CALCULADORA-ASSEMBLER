d17c6d56f2dfb6475e10c69c053b0221f66832c7
cmoralest4 <cmoralest4@miumg.edu.gt>
2021-09-01 21:53:12 -0600

    Creacion del Menu Principal

--- prueba.asm
+++ prueba.asm
@@ -1,18 +1,16 @@
-.model small
-.stack
-.data 
-    msg db "Hola UMG", "$"
-.code
+                                                                                                                                             
+org 100h
 
-main proc
-    mov ax,seg @data
-    mov ds, ax
-    
-    mov ah,09
-    lea dx, msg
-    int 21h
-    
-    mov ax,4c00h
-    int 21h
-    main endp
-end main
+.MODEL SMALL
+.STACK 100H
+.DATA
+MSG1 DB 'Para Suma presione   :'1'$'
+MSG2 DB 10,13,'Para Resta presione   :'2'$'
+MSG3 DB 10,13,'Fara Multiplicacion presione   :'3'$'
+MSG4 DB 10,13,'Para Division Presione   :'4'$'
+MSG5 DB 10,13,'Seleccione una opcion:$'
+MSG6 DB 10,13,10,13,'Ingrese Primer Numero:$'
+MSG7 DB 10,13,'Ingrese Segundo Numero:$'
+MSG8 DB 10,13,10,13,'El Resultado es:$' 
+MSG DB 10,13,10,13,'               ***Gracias por su atencion GRUPO #4***$'
+                           
